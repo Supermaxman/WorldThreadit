7c72508fe85a2ee85fa6e1832c2b0b28a8da5614
Max <supermaxman1@gmail.com>
2012-06-11 11:57:05 -0500

    Update

--- WorldThreadit/src/me/supermaxman/worldthreadit/WorldThreaditSet.java
+++ WorldThreadit/src/me/supermaxman/worldthreadit/WorldThreaditSet.java
@@ -3,28 +3,28 @@ package me.supermaxman.worldthreadit;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.World;
 import org.bukkit.block.Block;
 
 public class WorldThreaditSet extends Thread {
 	
-	private List<Block> blocks = new ArrayList<Block>();
 	
-	private int ItemID;
-	
-	public WorldThreaditSet(List<Block> b, int i){ blocks = b;ItemID=i;}
-    public void run() {
-        Thread t = Thread.currentThread();
-		while (!t.isInterrupted()) {
-		    for (Block b : blocks) {
-		        b.setTypeId(ItemID);
-		        try {
-					sleep(1);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+		private List<Block> blocks = new ArrayList<Block>();
+
+		private int ItemID;
+
+		public WorldThreaditSet(List<Block> b, int i){ blocks = b;ItemID=i;}
+		    public void run() {
+		        Thread t = Thread.currentThread();
+		        World world = null;
+		        if(world == null){
+		            	world = blocks.get(0).getWorld();
+		            }
+		            world.setAutoSave(false);
+		            for (Block b : blocks) {
+		            	b.setTypeId(ItemID);
+		            	
+		            }
+		            world.setAutoSave(true);
 		    }
-		    this.interrupt();
-		}
-    }
-    
 }
