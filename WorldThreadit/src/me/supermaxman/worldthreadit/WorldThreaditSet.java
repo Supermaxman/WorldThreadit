5537b2590bc4cc4f4431622daadce401b06bc93b
Benjamin James Harrison-Sims <tehrainbowguy@gmail.com>
2012-06-12 05:44:57 +0100

    fixes and such

--- WorldThreadit/src/me/supermaxman/worldthreadit/WorldThreaditSet.java
+++ WorldThreadit/src/me/supermaxman/worldthreadit/WorldThreaditSet.java
@@ -1,51 +1,68 @@
 package me.supermaxman.worldthreadit;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Chunk;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-
-import java.util.List;
+import org.bukkit.util.Vector;
 
 public class WorldThreaditSet extends Thread {
 
-private final List<Block> blocks;
+    private final Vector min;
+    private final Vector max;
+    private final Material mat;
 
-private final Material ItemID;
+    private final Player player;
 
-private final Player player;
-
-public WorldThreaditSet(List<Block> b, Material i, Player p){
-    setName("WT-Thread-"+getId());
-    blocks = b;
-    ItemID=i;
-    player = p;
-}
-    public void run() {
-        if(blocks != null){
-        World world = null;
-            if(world == null){
-                world = blocks.get(0).getWorld();
-            }
-            world.setAutoSave(false);
-            for (Block b : blocks) {
-             if(b != null){
-                    final Chunk c = b.getChunk();
-
-                    if(!c.isLoaded()){
-                       c.load();
-                    }
-                b.setType(ItemID);
-             }
-            }
-            world.setAutoSave(true);
-
-
-            player.sendMessage(ChatColor.AQUA+"[WorldThredit] "+ChatColor.GREEN + blocks.size()+" Block Edit.");
-			System.out.println("[WorldThredit]  "+ blocks.size()+" Block Edit By "+player.getName());
-        }
+    public WorldThreaditSet(Vector mn, Vector mx, Material i, Player p) {
+        setName("WT-Thread-" + getId());
+        mat = i;
+        player = p;
+        min = mn;
+        max = mx;
     }
-    
+
+    public void run() {
+        World world = player.getWorld();
+        int i = 0;
+        for (int x = (int) min.getX(); x <= (int) max.getX(); x++) {
+            for (int z = (int) min.getZ(); z <= (int) max.getZ(); z++) {
+                for (int y = (int) min.getY(); y <= (int) max.getY(); y++) {
+
+                    //counting run
+                    i++;
+                }
+            }
+        }
+        world.setAutoSave(false);
+        player.sendMessage(ChatColor.AQUA + "[WorldThredit] " + ChatColor.GREEN + i + " Block Edit.");
+        System.out.println("[WorldThredit]  " + i + " Block Edit By " + player.getName());
+        for (int x = (int) min.getX(); x <= (int) max.getX(); x++) {
+            for (int z = (int) min.getZ(); z <= (int) max.getZ(); z++) {
+                for (int y = (int) min.getY(); y <= (int) max.getY(); y++) {
+
+                    //Get block and null check
+                    Block b = player.getWorld().getBlockAt(x, y, z);
+                    if (b != null) {
+                        //Get location
+                        Location l = b.getLocation();
+                        //Check height
+                        if (!(l.getBlockY() < 0 || l.getBlockY() > 256)) {
+                            final Chunk c = b.getChunk();
+                            //Load chunk, fixes npe
+                            if (!c.isLoaded()) {
+                                c.load();
+                            }
+                            //Set block!
+                            b.setType(mat);
+                        }
+                    }
+                }
+            }
+        }
+        world.setAutoSave(true);
+
+        player.sendMessage(ChatColor.AQUA + "[WorldThredit] " + ChatColor.GREEN + "Done!");
+
+    }
+
 }
