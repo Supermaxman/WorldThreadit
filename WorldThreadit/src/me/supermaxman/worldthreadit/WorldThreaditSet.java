12ae66b09c75ca2af9294e2bc6a4ad1117cf9768
Benjamin James Harrison-Sims <tehrainbowguy@gmail.com>
2012-07-09 22:40:15 +0100

    derp

--- WorldThreadit/src/me/supermaxman/worldthreadit/WorldThreaditSet.java
+++ WorldThreadit/src/me/supermaxman/worldthreadit/WorldThreaditSet.java
@@ -1,9 +1,6 @@
 package me.supermaxman.worldthreadit;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Chunk;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
@@ -29,27 +26,45 @@ public class WorldThreaditSet extends Thread {
         final World world = player.getWorld();
         int i = 0;
         final ArrayList<Chunk> chunks = new ArrayList<Chunk>();
+        final ArrayList<Block> blocks = new ArrayList<Block>();
         world.setAutoSave(false);
             for (int x = (int) min.getX(); x <= (int) max.getX(); x++) {
                 for (int z = (int) min.getZ(); z <= (int) max.getZ(); z++) {
                     for (int y = (int) min.getY(); y <= (int) max.getY(); y++) {
-                        //Cache run
                     final Block b = world.getBlockAt(x, y, z);
                     if (!(y <= 0 && y >= 256)) {
-                        final Chunk c = b.getChunk();
-                        if(!chunks.contains(c)){
-                            if (!c.isLoaded()) {
-                                c.load();
-                            }
-                            chunks.add(c);
-                        }
-                        //Set block!
-                        b.setType(mat);
+                      blocks.add(b);
                     }
                     i++;
                 }
             }
         }
+        for (Block b : blocks) {
+            final Chunk c = b.getChunk();
+            if(!chunks.contains(c)){
+                if (!c.isLoaded()) {
+                    c.load();
+                }
+                chunks.add(c);
+            }
+        }
+        int i2 = 0;
+        final int id = mat.getId();
+        for (Block block : blocks) {
+            i++;
+            //Set block!
+            block.setTypeId(id);
+            if(i2 >= 10){
+                i2 = 0;
+                try {
+                    sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
+
+        }
+
         world.setAutoSave(true);
 
         player.sendMessage(ChatColor.AQUA + "[WorldThredit] " + ChatColor.GREEN + i + " Block Edit.");
